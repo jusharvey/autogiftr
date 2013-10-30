464d2509e5e48a3344ee1fb134697dabfb4349b0
Justin Harvey <justinharvey@Justins-MacBook-Air.local>
2013-10-30 20:28:51 +1100

    scaffold recipient

---
+++ app/views/recipients/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipient, :name, :relationship, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Autogiftr::Application.routes.draw do
+  resources :recipients
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
